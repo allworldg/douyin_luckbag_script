9f13e2bd9db54cfed6b344c3185803b95b04d0ed
allworldg <944937881@qq.com>
2025-09-09 10:27:22 +0800

    backup

--- main.js
+++ main.js
@@ -1,17 +1,15 @@
+setScreenMetrics(2160,1080)
+
 let founded = launchApp("抖音")
 if(!founded){
     toastLog("未找到抖音")
 }
 let btnObject = desc("团购，按钮").findOne(10000)
-toastLog(btnObject)
 if(btnObject==null){
     toastLog("无法找到 团购 按钮，请联系管理员")
 }
 btnObject.click()
-
-
-
-
+sleep(3000)
 
 
 

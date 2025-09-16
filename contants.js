7377f60e123e9e8bb2ca77a0cbc62c0588b4d304
allworldg <944937881@qq.com>
2025-09-16 11:40:05 +0800

    feat: add skip live room(click the empty postion)

--- contants.js
+++ contants.js
@@ -1,4 +1,14 @@
 module.exports = Object.freeze({
-  MAX_COUNTDOWN_SECONDS: 480,
-  MAX_AUDIENCE_COUNT: 200,
+  MAX_COUNTDOWN_SECONDS: 600,
+  MAX_AUDIENCE_COUNT: 60,
+  MAX_AUDIENCE_RATIO: 100 / 3,
+  TEXT_CLASSNAME: "com.lynx.tasm.behavior.ui.text.FlattenUIText",
+  CLICKABLE_EMPTY_X: {
+    INITIAL:300,
+    OFFSET:200
+  },
+  CLILCKABLE_EMPTY_Y:{
+    INITIAL:550,
+    OFFSET:50
+  }
 });
--- main.js
+++ main.js
@@ -1,6 +1,13 @@
 auto();
 setScreenMetrics(1080, 2160);
-const { MAX_COUNTDOWN_SECONDS,MAX_AUDIENCE_COUNT } = require("./contants");
+const {
+  MAX_COUNTDOWN_SECONDS,
+  MAX_AUDIENCE_COUNT,
+  TEXT_CLASSNAME,
+  MAX_AUDIENCE_RATIO,
+  CLILCKABLE_EMPTY_Y,
+  CLICKABLE_EMPTY_X,
+} = require("./contants");
 
 console.setGlobalLogConfig({
   file: files.path("./log/log.txt"),
@@ -22,7 +29,7 @@ if (audience_widget == null) {
 }
 const audienceCountText = audience_widget.getText();
 click(find_luckbag_result.x, find_luckbag_result.y);
-let timeWidget = className("com.lynx.tasm.behavior.ui.text.FlattenUIText")
+let timeWidget = className(TEXT_CLASSNAME)
   .textContains(":")
   .depth(14)
   .findOne(2000);
@@ -30,16 +37,34 @@ if (timeWidget == null) {
   toastLog("cannot get countdown, will exit...");
   exit();
 }
-let countdownText = timeWidget.text();
-const array_countdown = countdownText.split(":")
-const countdownTime = parseInt(array_countdown[0])*60+parseInt(array_countdown[1])
-const audienceCount = parseInt(audienceCountText)
-if(countdownTime>MAX_COUNTDOWN_SECONDS||audienceCount>MAX_AUDIENCE_COUNT){
-  // do nothing
-}else{
-  
-}
+//300 500 ~ 550 600
+let luckyBag_count_text = className(TEXT_CLASSNAME)
+  .textStartsWith("限量")
+  .findOne(2000)
+  .text();
 
+let luckybag_count = parseInt(
+  luckyBag_count_text.substring(2, luckyBag_count_text.length - 1)
+); //todo
+let countdownText = timeWidget.text();
+const array_countdownText = countdownText.split(":");
+const countdownTime =
+  parseInt(array_countdownText[0]) * 60 + parseInt(array_countdownText[1]);
+const audienceCount = parseInt(audienceCountText);
+if (
+  countdownTime > MAX_COUNTDOWN_SECONDS ||
+  (audienceCount > MAX_AUDIENCE_COUNT &&
+    audienceCount / luckybag_count > MAX_AUDIENCE_RATIO)
+) {
+  // Todo: skip this live room
+  let click_x = Math.floor(
+    Math.random() * CLICKABLE_EMPTY_X.OFFSET + CLICKABLE_EMPTY_X.INITIAL
+  );
+  let click_y = Math.floor(
+    Math.random() * CLILCKABLE_EMPTY_Y.OFFSET + CLILCKABLE_EMPTY_Y.INITIAL
+  );
+  click(click_x,click_y)
+}
 
 function findLuckBag() {
   let region = [25, 310, 200, 200];
@@ -54,4 +79,3 @@ function findLuckBag() {
   });
   return result;
 }
-
